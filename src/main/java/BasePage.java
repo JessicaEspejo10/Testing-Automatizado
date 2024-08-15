23d1763c0126ac1bd7d1c1697981340a16ce59a6
Jessica Espejo <jenespejoto@unal.edu.co>
2024-08-15 17:24:18 -0500

    BeforeEach y AfterEach anotaciones

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasePage {
 
@@ -9,6 +8,7 @@ public class BasePage {
 
     protected BasePage(WebDriver driver) { this.driver = driver; }
 
+<<<<<<< HEAD
     public void setup() {
         driver.manage().window().maximize();
     }
@@ -16,15 +16,28 @@ public class BasePage {
     public void url(String url) throws InterruptedException {
         Thread.sleep(1000);
         driver.get(url); }
+=======
+    public void url(String url) {
+        driver.get(url);
+    }
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
 
     public void close() { driver.quit(); }
 
+<<<<<<< HEAD
     protected WebElement findElement(By locator) { return driver.findElement(locator); }
 
     protected void sendText(String inputText, By locator ) throws InterruptedException {
         Thread.sleep(1000);
+=======
+    protected WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void sendText(String imputText, By locator ) {
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
         this.findElement(locator).clear();
-        this.findElement(locator).sendKeys(inputText);
+        this.findElement(locator).sendKeys(imputText);
     }
 
     protected void sendKey(CharSequence key, By locator) throws InterruptedException {
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,36 +1,71 @@
+<<<<<<< HEAD
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+=======
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
 
 public class SearchTest {
 
     public WebDriver driver;
 
     @BeforeEach
+<<<<<<< HEAD
     public void precondition() throws InterruptedException {
         driver = new ChromeDriver();
         SearchPage searchPage = new SearchPage(driver);
 
         searchPage.setup();
         searchPage.url("https://digital-booking-front.digitalhouse.com/");
+=======
+    public void preconditions() throws InterruptedException {
+        driver = new ChromeDriver();
+        SearchPage searchPage = new SearchPage(driver);
+        driver.manage().window().maximize();
+        searchPage.url("https://digital-booking-front.digitalhouse.com/");
+        Thread.sleep(1000);
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
     }
+
 
     @Test
     public void BusquedaExitosa_Grecia() throws InterruptedException {
         SearchPage searchPage = new SearchPage(driver);
+<<<<<<< HEAD
 
         searchPage.writeSearch("Paros");
+=======
+        searchPage.writeSearch("Paros");
+        Thread.sleep(1000);
+
         searchPage.clickSearch();
+        Thread.sleep(2000);
 
         searchPage.searchResult();
     }
 
+    @Test
+    public void BusquedaExitosa_Uruguay() throws InterruptedException {
+        SearchPage searchPage = new SearchPage(driver);
+        searchPage.writeSearch("Punta del este");
+        Thread.sleep(1000);
+
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
+        searchPage.clickSearch();
+        Thread.sleep(2000);
+
+        searchPage.searchResult();
+    }
+
+<<<<<<< HEAD
     @Test
     public void BusquedaExitosa_Uruguay() throws InterruptedException {
         SearchPage searchPage = new SearchPage(driver);
@@ -41,10 +76,16 @@ public class SearchTest {
         searchPage.searchResult();
     }
 
+=======
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
     @AfterEach
     public void close() {
         SearchPage searchPage = new SearchPage(driver);
         searchPage.close();
+<<<<<<< HEAD
+=======
+        driver.quit();
+>>>>>>> fe279fe (BeforeEach y AfterEach anotaciones)
     }
 
 }
