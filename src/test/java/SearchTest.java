ad9a031dc6cb8f979e651156e5c3c55a0b886208
Jessica Espejo <jenespejoto@unal.edu.co>
2024-08-15 19:26:13 -0500

    Generación de reportes

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import reports.ExtentFactory;
 
 import java.time.Duration;
 
@@ -13,9 +15,19 @@ public class SearchTest {
     public WebDriver driver;
     public WebDriverWait wait;
 
-    @BeforeEach
+    static ExtentSparkReporter info = new ExtentSparkReporter("target/Reporte.html");
+    static ExtentReports extent;
 
+    @BeforeAll
+    public static void createReport() {
+        //report
+        extent = ExtentFactory.getInstance();
+        extent.attachReporter(info);
+    }
+
+    @BeforeEach
     public void preconditions() throws InterruptedException {
+        //driver
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofMillis(3000));
         SearchPage searchPage = new SearchPage(driver, wait);
@@ -28,28 +40,43 @@ public class SearchTest {
     @Tag("BUSQUEDA")
     @Tag("SMOKE")
     public void BusquedaExitosa_Grecia() throws InterruptedException {
+        ExtentTest test = extent.createTest("Busqueda exitosa en Grecia");
+        test.log(Status.INFO, "Comienza nuestro test de busqueda");
+
         SearchPage searchPage = new SearchPage(driver, wait);
         searchPage.writeSearch("Paros");
-
         searchPage.clickSearch();
+        test.log(Status.PASS, "Se realizo la busqueda de Uruguay exitosamente");
+
         searchPage.searchResult();
+        test.log(Status.PASS,"Se valida que la busqueda ha sido exitosa");
     }
 
     @Test
     @Tag("BUSQUEDA")
     @Tag("REGRESSION")
     public void BusquedaExitosa_Uruguay() throws InterruptedException {
+        ExtentTest test = extent.createTest("Busqueda exitosa en Uruguay");
+        test.log(Status.INFO, "Comienza nuestro test de busqueda");
+
         SearchPage searchPage = new SearchPage(driver, wait);
         searchPage.writeSearch("Punta del este");
-
         searchPage.clickSearch();
+        test.log(Status.PASS, "Se realizo la busqueda de Uruguay exitosamente");
+
         searchPage.searchResult();
+        test.log(Status.PASS,"Se valida que la busqueda ha sido exitosa");
     }
 
     @AfterEach
     public void close() {
         SearchPage searchPage = new SearchPage(driver, wait);
         searchPage.close();
+    }
+
+    @AfterAll
+    public static void saveReport() {
+        extent.flush();
     }
 
 }
